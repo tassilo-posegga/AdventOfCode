f23c331db312c93f391d39c5356b29bff90a8e4f
Tassilo Posegga <tassilo@posegga.eu>
2022-12-17 19:34:49 +0100

    Day 8 Part 2

--- src/main/kotlin/day8/Day8.kt
+++ src/main/kotlin/day8/Day8.kt
@@ -9,64 +9,90 @@ import splitOnLineBreak
 fun main(args: Array<String>) {
     val input = "https://pastebin.com/raw/jcgLjS4C".getTextFromUrl().splitOnLineBreak()
     val forest = ForestParser.parseForest(input)
-    println(TreeObserver.getAmountOfVisibleTrees(forest))
+    val treeObserver = TreeObserver(forest)
+    println("Visible trees: ${treeObserver.getAmountOfVisibleTrees()}")
+    println("Highest scenic score: ${treeObserver.getHighestScenicScore()}")
 }
 
-object TreeObserver {
+class TreeObserver(private val forest: Forest) {
 
-    fun getAmountOfVisibleTrees(forest: Forest): Int {
-        var visibleTrees = 0
-        forest.trees.forEachIndexed { row, trees ->
-            trees.forEachIndexed { column, _ ->
-                if (isTreeVisible(row, column, forest))
-                    visibleTrees++
-            }
+    fun getHighestScenicScore(): Int =
+        forest.trees.maxOf { it.maxOf { tree -> getTreesScenicScore(tree) } }
+
+    fun getTreesScenicScore(tree: Tree): Int =
+        Direction.values().map {
+            getNumberTreesUntilBlocked(tree, getVisibleTreesInDirectionOf(tree, it))
+        }.multipliesOf()
+
+    private fun List<Int>.multipliesOf(): Int {
+        var multiplies = this[0]
+        for (i in 1 until size) {
+            multiplies *= this[i]
         }
-        return visibleTrees
+        return multiplies
     }
 
-    fun isTreeVisible(row: Int, column: Int, forest: Forest): Boolean {
-        val currentHeight = forest.trees[row][column].height
-        val isVisibleFromLeft = getIsVisibleFromLeft(forest, row, column, currentHeight)
-        val isVisibleFromRight = getIsVisibleFromRight(forest, row, column, currentHeight)
-        val isVisibleFromTop = getIsVisibleFromTop(forest, row, column, currentHeight)
-        val isVisibleFromBottom = getIsVisibleFromBottom(forest, row, column, currentHeight)
-        return isVisibleFromLeft || isVisibleFromRight || isVisibleFromTop || isVisibleFromBottom
+    fun getAmountOfVisibleTrees(): Int =
+        forest.trees.sumOf { it.count { tree -> isTreeVisible(tree) } }
+
+    fun isTreeVisible(tree: Tree): Boolean =
+        isVisibleFromDirection(tree, getVisibleTreesInDirectionOf(tree, Direction.Up))
+                || isVisibleFromDirection(tree, getVisibleTreesInDirectionOf(tree, Direction.Down))
+                || isVisibleFromDirection(tree, getVisibleTreesInDirectionOf(tree, Direction.Left))
+                || isVisibleFromDirection(tree, getVisibleTreesInDirectionOf(tree, Direction.Right))
+
+    fun getVisibleTreesInDirectionOf(tree: Tree, direction: Direction): List<Tree> =
+        when (direction) {
+            Direction.Left -> forest.trees[tree.row].subList(0, tree.column).reversed()
+            Direction.Right -> forest.trees[tree.row].subList(tree.column + 1, forest.trees[tree.row].size)
+            Direction.Up -> forest.trees.map { it[tree.column] }.subList(0, tree.row).reversed()
+            Direction.Down -> with(forest.trees.map { it[tree.column] }) { subList(tree.row + 1, size) }
+        }
+
+    private fun getNumberTreesUntilBlocked(tree: Tree, treesInDirection: List<Tree>): Int {
+        val indexOfFirstBlockingTree = treesInDirection.indexOfFirst { it.height >= tree.height }
+        //no blocking tree found, visible trees is all
+        return if (indexOfFirstBlockingTree == -1) {
+            treesInDirection.size
+        } else {
+            indexOfFirstBlockingTree + 1
+        }
     }
 
-    private fun getIsVisibleFromTop(forest: Forest, row: Int, column: Int, currentHeight: Int) =
-        forest.trees
-            .map { it[column] }
-            .subList(0, row)
+    private fun isVisibleFromDirection(tree: Tree, treesInDirection: List<Tree>): Boolean =
+        treesInDirection
             .map { it.height }
-            .none { it >= currentHeight }
-
-    private fun getIsVisibleFromBottom(forest: Forest, row: Int, column: Int, currentHeight: Int): Boolean {
-        val columns = forest.trees.map { it[column] }
-        return columns.subList(row + 1, columns.size)
-            .map { it.height }
-            .none { it >= currentHeight }
-    }
-
-    private fun getIsVisibleFromLeft(forest: Forest, row: Int, column: Int, currentHeight: Int) =
-        forest.trees[row].subList(0, column)
-            .map { it.height }
-            .none { it >= currentHeight }
-
-    private fun getIsVisibleFromRight(forest: Forest, row: Int, column: Int, currentHeight: Int) =
-        forest.trees[row].subList(column + 1, forest.trees[row].size)
-            .map { it.height }
-            .none { it >= currentHeight }
+            .none { it >= tree.height }
 }
+
 
 object ForestParser {
     fun parseForest(input: List<String>): Forest =
-        Forest(trees = input.map { stringInput ->
+        Forest(trees = input.mapIndexed { row, stringInput ->
+            // single chars as items
             stringInput.split("")
                 .filterNot { it.isEmpty() }
-                .map { Tree(height = it.toInt()) }
+                .mapIndexed { column, height ->
+                    Tree(
+                        height = height.toInt(),
+                        row = row,
+                        column = column,
+                    )
+                }
         })
 }
 
-class Tree(val height: Int)
+class Tree(
+    val height: Int,
+    val row: Int,
+    val column: Int
+)
+
 class Forest(val trees: List<List<Tree>>)
+
+enum class Direction {
+    Up,
+    Left,
+    Down,
+    Right,
+}
--- src/test/kotlin/day8/Day8KtTest.kt
+++ src/test/kotlin/day8/Day8KtTest.kt
@@ -2,12 +2,22 @@ package day8
 
 import org.junit.Test
 
-import org.junit.Assert.*
+import org.junit.Before
+import kotlin.test.assertEquals
+import kotlin.test.assertFalse
+import kotlin.test.assertTrue
 
 class Day8KtTest {
 
     private val forest = ForestParser.parseForest(input)
     private val trees = forest.trees
+
+    private lateinit var treeObserver: TreeObserver
+
+    @Before
+    fun setup() {
+        treeObserver = TreeObserver(forest)
+    }
 
     @Test
     fun testParsedCorrectly() {
@@ -23,27 +33,79 @@ class Day8KtTest {
     @Test
     fun testAllEdgeTreesVisible() {
         for (row in 0..4) {
-            assertTrue(TreeObserver.isTreeVisible(row, 0, forest))
-            assertTrue(TreeObserver.isTreeVisible(row, 4, forest))
+            assertTrue(treeObserver.isTreeVisible(trees[row][0]))
+            assertTrue(treeObserver.isTreeVisible(trees[row][4]))
         }
 
         for (column in 0..4) {
-            assertTrue(TreeObserver.isTreeVisible(0, column, forest))
-            assertTrue(TreeObserver.isTreeVisible(4, column, forest))
+            assertTrue(treeObserver.isTreeVisible(trees[0][column]))
+            assertTrue(treeObserver.isTreeVisible(trees[4][column]))
         }
     }
 
     @Test
     fun testIsTreeNotVisible() {
-        assertFalse(TreeObserver.isTreeVisible(2, 2, forest))
-        assertFalse(TreeObserver.isTreeVisible(1, 3, forest))
-        assertFalse(TreeObserver.isTreeVisible(3, 1, forest))
-        assertFalse(TreeObserver.isTreeVisible(3, 3, forest))
+        assertFalse(treeObserver.isTreeVisible(trees[2][2]))
+        assertFalse(treeObserver.isTreeVisible(trees[1][3]))
+        assertFalse(treeObserver.isTreeVisible(trees[3][1]))
+        assertFalse(treeObserver.isTreeVisible(trees[3][3]))
     }
 
     @Test
     fun testAmountOfVisibleTrees() {
-        assertEquals(21, TreeObserver.getAmountOfVisibleTrees(forest))
+        assertEquals(
+            expected = 21,
+            actual = treeObserver.getAmountOfVisibleTrees()
+        )
+    }
+
+    @Test
+    fun testGetScenicScoreForTree() {
+        assertEquals(
+            expected = 8,
+            actual = treeObserver.getTreesScenicScore(trees[3][2])
+        )
+
+        assertEquals(
+            expected = 0,
+            actual = treeObserver.getTreesScenicScore(trees[0][0])
+        )
+
+        assertEquals(
+            expected = 1,
+            actual = treeObserver.getTreesScenicScore(trees[2][2])
+        )
+    }
+
+    @Test
+    fun testGetHighestScenicScore() {
+        assertEquals(
+            expected = 8,
+            actual = treeObserver.getHighestScenicScore()
+        )
+    }
+
+    @Test
+    fun testGetVisibleTreesInDirection() {
+        assertEquals(
+            expected = listOf(trees[2][2], trees[1][2], trees[0][2]),
+            actual = treeObserver.getVisibleTreesInDirectionOf(trees[3][2], Direction.Up)
+        )
+
+        assertEquals(
+            expected = listOf(trees[3][1], trees[3][0]),
+            actual = treeObserver.getVisibleTreesInDirectionOf(trees[3][2], Direction.Left)
+        )
+
+        assertEquals(
+            expected = listOf(trees[4][2]),
+            actual = treeObserver.getVisibleTreesInDirectionOf(trees[3][2], Direction.Down)
+        )
+
+        assertEquals(
+            expected = listOf(trees[3][3], trees[3][4]),
+            actual = treeObserver.getVisibleTreesInDirectionOf(trees[3][2], Direction.Right)
+        )
     }
 
     companion object {
